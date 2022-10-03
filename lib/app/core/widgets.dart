6fdc0e45c6e784532a1be96aeae237217204bc46
sayanth A <sayanthsayu619@gmail.com>
2022-10-03 18:45:21 +0530

    const widget added

---
+++ lib/app/core/widgets.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+const height10 = SizedBox(
+  height: 10,
+);
+
+const height30 = SizedBox(
+  height: 30,
+);
+const height50 = SizedBox(
+  height: 30,
+);
