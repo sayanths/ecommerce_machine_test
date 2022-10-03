e9e3c22290915dd64ac511e3de47adc216261f43
sayanth A <sayanthsayu619@gmail.com>
2022-10-03 17:17:39 +0530

    cart page addec

---
+++ lib/app/cart/view/cart.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ReviwCart extends StatelessWidget {
+  const ReviwCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
