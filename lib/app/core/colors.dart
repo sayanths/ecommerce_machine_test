3d0cde90673363a33413cfe8e94625639d665eba
sayanth A <sayanthsayu619@gmail.com>
2022-10-03 18:43:44 +0530

    global variables added

---
+++ lib/app/core/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const black = Colors.black;
+const blue = Color.fromARGB(255, 0, 65, 119);
+const white = Colors.white;
+const green = Color.fromARGB(255, 0, 155, 5);
+const orange = Color.fromARGB(255, 197, 118, 0);
