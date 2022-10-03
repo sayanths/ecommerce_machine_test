ef047836bd1a73c5f057a7fd0d35177bef585333
sayanth A <sayanthsayu619@gmail.com>
2022-10-03 16:03:05 +0530

    carosuel ui added

--- lib/app/home_page/view/home_page.dart
+++ lib/app/home_page/view/home_page.dart
@@ -53,6 +53,23 @@ class HomePage extends StatelessWidget {
                 child: Text("Delivery by oct 5",textAlign: TextAlign.start,),
               ),
             ),
+             CarouselSlider(
+  options: CarouselOptions(height: 180.0),
+  items: [1,2,3,4,5].map((i) {
+    return Builder(
+      builder: (BuildContext context) {
+        return Container(
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
+          decoration: BoxDecoration(
+            color: Colors.amber
+          ),
+         /// child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+        );
+      },
+    );
+  }).toList(),
+)
           ],
         ),
       ),
